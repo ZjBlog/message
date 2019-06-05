fa373737547d047af42f8aae3f6046a405077f25
暴躁DPS <13552960783@163.com>
2019-06-05 12:24:01 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -l
 echo '👍 ENTRYPOINT HAS STARTED—INSTALLING THE GEM BUNDLE'
-curl $URL+'?text='+$MESSAG+'\&desp='+${GITHUB_SHA}
+curl $URL+'?text='+$MESSAGE+'\&desp='+${GITHUB_SHA}
 echo '👍 GREAT SUCCESS!'
